d60844665c820c820dce96ed2014b54061ba6fcc
GilaniF <farzan.iqgilani@gmail.com>
2023-07-13 19:21:11 -0600

    added file for footer style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 //import Signup from './views/signup.js';
 import Header from './views/header.js';
+import Footer from './views/footer.js';
 //import Footer from './views/footer.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header></Header>
+        <Footer></Footer>
+
       </header>
     </div>
   );
