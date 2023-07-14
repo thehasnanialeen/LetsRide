db70613783864a07b809ae128d6f527837a42c5a
GilaniF <farzan.iqgilani@gmail.com>
2023-07-14 00:15:51 -0600

    Header top bar

--- frontend/src/views/header.js
+++ frontend/src/views/header.js
@@ -1,19 +1,39 @@
 import React from "react";
 
-const Header = () => {
-    return(
-        <div className="header">
-            <div className="logo">
-                <h1>MovieCity</h1>
-            </div>
-            <nav className="navigation">
-                <ul>
-                    <li><a href="/">Let's ride </a></li>
-                    <li><a href="/">Logo</a></li>
-                </ul>
-                <button type="button">SignUp</button>
-            </nav>
+  
+const Footer = () => {
+  return (
+    <body>
+    <nav>
+        <div class="heading">Landing Page</div>
+        <span class="sideMenuButton" 
+            onclick="openNavbar()">
+            ☰
+        </span>
+  
+        <div class="navbar">
+            <ul>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
         </div>
-    );
-}
-export default Header;
+    </nav>
+  
+    {/* <!-- Side navigation bar for 
+        responsive website --> */}
+    <div class="sideNavigationBar" 
+        id="sideNavigationBar">
+        <a href="#" class="closeButton" 
+            onclick="closeNavbar()">
+            ❌
+        </a>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Sign Up</a>
+    </div>
+    </body>
+  
+  );
+};
+export default Footer;
