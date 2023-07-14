870a29c2c720ad099431f0386b4515f4b8ce56e7
GilaniF <farzan.iqgilani@gmail.com>
2023-07-13 19:11:03 -0600

    added code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 //import Signup from './views/signup.js';
 import Landing from './views/landing.js';
+import Footer from './views/footer.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Landing></Landing>
+        <Footer></Footer>
       </header>
     </div>
   );
