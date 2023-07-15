768f83008ca21e8e14d4a10c77acabcac99ea541
GilaniF <farzan.iqgilani@gmail.com>
2023-07-15 15:01:59 -0600

    library install

--- frontend/src/views/footer.js
+++ frontend/src/views/footer.js
@@ -1,53 +1,62 @@
-// import React from "react";
-
-// const Footer = () => {
-//     return(
-//         <div className="header">
-//             <div className="logo">
-//                 <h1>MovieCity</h1>
-//             </div>
-//             <nav className="navigation">
-//                 <ul>
-//                     <li><a href="/">Let's ride </a></li>
-//                     <li><a href="/">Logo</a></li>
-//                 </ul>
-//                 <button type="button">SignUp</button>
-//             </nav>
-//         </div>
-//     );
-// }
-// export default Footer;
-
 import React from "react";
-import '../App.css';
-  
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faYoutube,
+    faGithub,
+    faFacebook,
+    faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import './footer.css';
+import Photo from './images/logo.svg';
+
 const Footer = () => {
-  return (
-    <body className="bodyfoot">
-      <Container className="containerfoot">
-        <Row className="rowfoot">
-          <Column className="colfoot">
-            <Heading className="headfoot"> About Us</Heading>
-            <FooterLink className="linkfoot" href="">Team</FooterLink>
-            <FooterLink className="linkfoot"> Vision </FooterLink>
-            <FooterLink className="linkfoot"> Mission </FooterLink>
-            <FooterLink className="linkfoot"> Contact: 1 800-757-438 </FooterLink>
-          </Column>
-          <Column>
-            <Heading className="headfoot">Corporate</Heading>
-            <FooterLink className="linkfoot"  href="#">Careers</FooterLink>
-            <FooterLink className="linkfoot" href="#">Reports</FooterLink>
-            <FooterLink className="linkfoot" href="#">Partners</FooterLink>
-          </Column>
-          <Column>
-            <Heading className="headfoot">Socials</Heading>
-            <FooterLink className="linkfoot" href="#">Facebook</FooterLink>
-            <FooterLink className="linkfoot" href="#">Instagram</FooterLink>
-            <FooterLink className="linkfoot" href="#">Twitter</FooterLink>
-          </Column>
-        </Row>
-      </Container>
-    </body>
-  );
-};
-export default Footer;
+    return (
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="company-info">
+            <img src={Photo} alt="Company Logo" className="logo" />
+            {/* <h3 className="company-name">FastForward</h3> */}
+          </div>
+          <div className="item2">
+            <span className="copyright">
+              <span className="text">©</span>{" "}
+              {new Date().getFullYear()} FarmForward. All Rights Reserved.
+            </span>
+          </div>
+          <div className="social-icons">
+            <a
+              href="https://github.com/noahstaz/AgTech2023.git"
+              target="_blank"
+              className="icon-link"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a
+              href="http://fb.com"
+              target="_blank"
+              className="icon-link"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              className="icon-link"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a
+              href="https://www.youtube.com/"
+              target="_blank"
+              className="icon-link"
+            >
+              <FontAwesomeIcon icon={faYoutube} />
+            </a>
+            
+          </div>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
