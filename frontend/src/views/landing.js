5243a4e0e461b71244b624fbedd0737e38dfcc30
GilaniF <farzan.iqgilani@gmail.com>
2023-07-15 11:22:04 -0600

    logo change

---
+++ frontend/src/views/landing.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Body.css'; // assuming you have a separate CSS file for styling
+import logo from './logo.png';
+import testimonialImage from './testimonial.png';
+
+const Land = () => {
+  return (
+    <div className="body">
+      <div className="first-section">
+        <div className="left-side">
+          <img src={testimonialImage} alt="Testimonial" className="testimonial-image" />
+        </div>
+        <div className="right-side">
+          <div className="quote">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          </div>
+          <div className="buttons">
+            <button className="login-button">Login</button>
+            <button className="signup-button">Sign Up</button>
+          </div>
+        </div>
+      </div>
+      <div className="second-section">
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="container">
+          <div className="vision-container">
+            <h2>Vision</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div className="mission-container">
+            <h2>Mission</h2>
+            <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Land;
