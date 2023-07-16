7f448ccbacc776df1bd82276265821aaa68e238b
thehasnanialeen <aleen.hasnani89@gmail.com>
2023-07-15 20:24:34 -0600

    signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Landing from './views/landing.js';  
-import Header from './views/header.js'; 
-//import Signup from './views/signup.js'; 
+import Signup from './views/signup.js'; 
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={OnboardingScreen} />
-        <Route path="/finance" component={Finance} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/tutorials" component={TutorialScreen} />
-        <Route exact path="/equipment" component={EquipmentPage} />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/signup" component={Signup} />
       </Switch>
-    
     </BrowserRouter>
   );
 }
