2f8d1f3e19b77cfb8df977fa496073bcb9aa4b00
thehasnanialeen <103472676+thehasnanialeen@users.noreply.github.com>
2023-07-18 17:00:31 -0600

    integrated frontend and backend

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -43,6 +43,8 @@ const authenticationController = {
         return res.status(401).json({ message: 'Invalid password' });
       }
 
+      req.session.user = user;
+
       res.status(200).json({ message: 'Login successful', user });
     } catch (error) {
       res.status(500).json({ message: 'Error logging in' });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require("express");
 const dotenv  = require("dotenv").config();
 const mongoose = require("mongoose");
+const session = require('express-session');
 
 //App Routes
 const authenticationRoutes = require('./routes/authenticationRoutes');
@@ -16,6 +17,13 @@ const app = express();
 
 //app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(express.json());
+app.use(
+  session({
+    secret: 'WeAreRegisteredInCs476', // Replace with your secret key for session encryption
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 mongoose.connect(
     "mongodb+srv://aleenhasnani:DUqqgA7LoCgp1Jgp@letsride.dt7jgdg.mongodb.net/LetsRideDatabase?retryWrites=true&w=majority", 
@@ -37,6 +45,11 @@ app.use('/api/driverRegistration', driverRegistrationRoutes);
 app.use('/api/rating', ratingRoutes);
 app.use('/api/rideDetails', rideDetailsRoutes);
 app.use('/api/rideDistancePrice', rideDistancePriceRoutes);
+
+app.get('/api/userSession', (req, res) => {
+  const user = req.session.user; // Retrieve the user's details from the session
+  res.json(user);
+});
 
 app.listen(port, () => {
     console.log(`running on port ${port}`);
